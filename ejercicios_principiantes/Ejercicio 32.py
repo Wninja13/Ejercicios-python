1c9e519021906de8bf4cd67bff35cef64e42f712
German Weckesser <german.weckesser@gmail.com>
2024-01-22 13:18:22 -0300

    NC

---
+++ ejercicios_principiantes/Ejercicio 32.py
@@ -0,0 +1,1 @@
+# Escribir un  programa que cree la variable L y le asigne la lista L=[3,6,9,12,15,18,21,24]. Luego, crear una nueva lis L1, mediante una comprension de lista que contenga los numeros de L divididos por 3.  El programa debe mostrar la lisla L1 en la consola. 

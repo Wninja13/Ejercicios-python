ad731ffe725230ebbae303a7d60c75701c3d8068
German Weckesser <german.weckesser@gmail.com>
2024-01-25 00:50:16 -0300

    NC

--- ejercicios_intermedios/Ejercicio 34.py
+++ ejercicios_intermedios/Ejercicio 34.py
@@ -1,3 +1,8 @@
 # Escribe una funcion llamada f(a,b,x) tomando como parametros 3 enteros a,b,x y devuelve el resultado de la siguiente funcion:
 # f(a,b,x)=a*(x**3)+2*a*(x**2)+b
 #Pruebas de verificacion: f(3,0,1)=9 y f(0,2,2)=2
+
+def f (a,b,x):
+    return a*(x**3)+2*a*(x**2)+b
+print(f(3,0,1))
+print(f(0,2,2))
